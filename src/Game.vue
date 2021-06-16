b0e457a72a9bb4f2fdb3e014ef3f65762a2e6d3f
IvanErmolenko <naviyear.ikota@gmail.com>
2021-06-16 15:34:40 +0300

    finishing the basics and start working with router

---
+++ src/Game.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "submit",
+    },
+    icon: {
+      type: Object,
+      validator(value) {
+        return value["right"] || value["left"] ? true : false;
+      },
+      isSpinner: {
+        type: Boolean,
+        default: true,
+      },
+      isReplace: {
+        type: Boolean,
+        default: true,
+      },
+    },
+  },
+};
+</script>
---
+++ src/views/EventList.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="events">
+    <h1>Events For Good</h1>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from "@/components/EventCard.vue";
+// import axios from "axios";
+import EventService from '../services/EventService';
+
+export default {
+  name: "EventList",
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+    };
+  },
+  created() {
+    EventService.getEvents()
+      .then((response) => {
+        console.log({ response });
+        this.events = response.data; 
+      })
+      .catch(error=>{
+        console.log(error);
+      });
+  },
+};
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
